81bf4b83339e1b07802c808f28b164dc5a881691
GIOVANNA GROSSI MONTEIRO <B51076@fgv.edu.br>
2023-08-18 08:45:22 -0300

    módulos em numpy

---
+++ modulo.py
@@ -0,0 +1,25 @@
+# Módulos
+
+## módulo simples p/ exemplo
+
+def saudação():
+    print("Eu adoro a EMAp!")
+    
+    
+var = "O Yuri é um grande homem"
+
+
+
+if __name__ == "__main__":
+   saudação()
+ 
+
+### um módulo é um arquivo com uma função específica que é importado para um outro aquivo central
+### time e numpy também são módulos
+
+
+
+
+
+
+
---
+++ teste_modulo.py
@@ -0,0 +1,17 @@
+import modulo
+
+print("Meu programa")
+modulo.saudação()
+print(dir(modulo)) #diz tudo o que existe no módulo
+
+ 
+
+
+
+
+
+
+
+
+
+
