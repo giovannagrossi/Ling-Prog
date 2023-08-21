ed25f1734f12cd6c7a63d185f7ecb3f3f0f8ac74
GIOVANNA GROSSI MONTEIRO <B51076@fgv.edu.br>
2023-08-21 08:31:14 -0300

    Sobre comentários

--- exercicios_21_08.py
+++ exercicios_21_08.py
@@ -1,20 +1,32 @@
 import numpy as np
 import numpy.random as npr
 
+"""
+Comentários:
+    Princípio da proximidade
+    Princípio da simplicidade
+    Princípio da não redundância
+    Princípio do código limpo
+    
+    * Site: pep8.org
+        "Como estilizar seu código"
+"""
+
 # Encontrar os valores comuns entre dois vetores
 
 a1d_1 = np.array(range(0,15))
 a1d_2 = np.array(range(10,20))
 
 array_intersect = np.intersect1d(a1d_1 , a1d_2)
+
 print(array_intersect)
 print("#"*50)
 
 # Em um vetor com 100 elementos (0-99), inverta os valores de 42 ate 66
 
 a1d_3 = np.array(range(0,100))
-a1d_3[(a1d_3>41) & (a1d_3<67)] *= -1 ## "c reduzido": a1d_3 = a1d_3 * -1
-## usamos filtro mas também dá pra usar slycing
+a1d_3[(a1d_3>41) & (a1d_3<67)] *= -1 ## "C reduzido": a1d_3 = a1d_3 * -1
+## Usamos filtro mas também dá pra usar slycing
 
 print(a1d_3)
 print("#"*50)
@@ -24,7 +36,7 @@ print("#"*50)
 a1d_4 = npr.randint(1000, size = (4,4))
 
 valor = npr.uniform(0,1000)
-## valor entre
+## Pega um valor entre 
 
 aprox = np.abs(a1d_4 - valor).argmin()
 ## abs pega o módulo do elemento e argmin pega o índice do menor valor
