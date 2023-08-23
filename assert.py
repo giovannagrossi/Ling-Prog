f70f24ed4d1a0cc7126d450cc53d6b4c0a025b31
GIOVANNA GROSSI MONTEIRO <B51076@fgv.edu.br>
2023-08-23 08:09:59 -0300

    Assert

---
+++ assert.py
@@ -0,0 +1,18 @@
+import documentation_module as dm
+
+numero = 42
+assert isinstance(numero, int),"Erro, tipo diferente recebido."
+#o assert debuga o código mas aumenta o tamanho do programa
+
+
+"""
+def soma_float(a,b):
+    assert isinstance(a, float), "Erro, tipo diferente recebido."
+    assert isinstance(b, float), "Erro, tipo diferente recebido."
+"""
+
+
+
+
+
+
